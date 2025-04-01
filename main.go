92c2bc69e4201e41685a6d4dee05d2ea41ca6fd5
TheJoker971 <jojo.blsegor@gmail.com>
2025-04-01 18:34:07 +0200

    Listen server

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import(
+import (
 	"fmt"
+	"net"
 )
 
+func handleConnection(conn net.Conn) {
+	conn.RemoteAddr()
+	defer conn.Close()
+}
+
+func listenServer(port uint) {
+	address := fmt.Sprintf(":%d", port)
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Erreur lors de l'écoute :", err)
+		return
+	}
+	fmt.Println("🚀 Serveur en écoute sur le port", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Erreur d'acceptation :", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World !")
+	listenServer(8080)
 }
