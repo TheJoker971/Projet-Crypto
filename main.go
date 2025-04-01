9a0201ea00af6de99846069bd75aa33a0f792ab0
TheJoker971 <jojo.blsegor@gmail.com>
2025-04-01 22:33:16 +0200

    Start request response

--- main.go
+++ main.go
@@ -5,17 +5,63 @@ import (
 	"net"
 	"net/http"
 	"io"
+	"encoding/json"
+	"log"
 )
 
+type Result struct {
+	Error []string `json:"error"`
+	Result struct {
+	}`json:"result"`
+}
+
+type ServerTime struct {
+	unixtime int `json:"unixtime"`
+	rfc1123 string `json:"rfc1123"`
+}
+
+type ServerSystemStatus struct {
+	status string `json:"status"`
+}
+
+type Tickers struct {
+	Pair []struct{
+		v []string `json:"v"`
+		h []string `json:"h"`
+		l []string `json:"l"`
+	}
+}
 
 func getPair(pair string) {
 	link := fmt.Sprintf("https://api.kraken.com/0/public/Ticker?pair=%s",pair)
 	resp, err := http.Get(link)
 	if err != nil {
-		// handle error
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
+
+	return body
+}
+
+func getAllPair() []byte{
+	resp, err := http.Get("https://api.kraken.com/0/public/Ticker")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	return body
+}
+
+func parseResponse( response []byte) Result {
+	var value Result
+	err := json.Unmarshal([]byte(body),&value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return value
 }
 
 func handleConnection(conn net.Conn) {
@@ -43,5 +89,7 @@ func listenServer(port uint) {
 }
 
 func main() {
+	pairs := getAllPair()
+	fmt.Println(parseResponse(pairs))
 	listenServer(8080)
 }
